818df056e8f35aed4fa1ef20098f870de9dfdba2
AndersLemme <a.lemme@hotmail.com>
2025-06-08 22:15:48 +0200

    Created grid with forloop. And added flexbox and wrapper to grid container

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("test");
+
+const grid = document.querySelector(".grid");
+for(let i = 0; i < 10*10; i++){
+    const grid_div = document.createElement("div");
+    grid_div.style.cssText = "background: gray; height: 20px; width: 20px; outline: 0.5px solid;";
+    grid.appendChild(grid_div);
+}
