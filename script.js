1077a926a1f2e631dbda2eee96b371c4a1ccb86f
AndersLemme <a.lemme@hotmail.com>
2025-06-11 22:55:08 +0200

    Added favicon and Header. started on buttons for changing colors

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+let dark = "#111820"
+let blue = "blue"
+
+let color = "#111820"
 
 const grid = document.querySelector(".grid");
 for(let i = 0; i < 10*10; i++){
     const grid_div = document.createElement("div");
     grid_div.style.cssText = "background: #D0B3FF; height: 50px; width: 50px; outline: 0.5px solid;";
     grid_div.addEventListener("mouseover", () =>{
-        grid_div.style.cssText = "background: #111820; height: 50px; width: 50px; outline: 0.5px solid;";
+        grid_div.style.cssText = `background: ${color}; height: 50px; width: 50px; outline: 0.5px solid;`;
         console.log("turn Black")
     } );
     grid.appendChild(grid_div);
