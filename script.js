5dfcc8c6582534b5aaad0270d6fe9c38cf931f02
AndersLemme <a.lemme@hotmail.com>
2025-06-16 13:12:23 +0200

    Added event, change color to buttons

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let dark = "#111820"
 let blue = "blue"
-
 let color = "#111820"
+
+const white_btn = document.getElementById("white");
+white_btn.addEventListener("click", () =>{ color = "#D0B3FF";});
+const black_btn = document.getElementById("black");
+black_btn.addEventListener("click", () =>{ color = "#111820";});
+
 
 const grid = document.querySelector(".grid");
 for(let i = 0; i < 10*10; i++){
@@ -9,7 +14,6 @@ for(let i = 0; i < 10*10; i++){
     grid_div.style.cssText = "background: #D0B3FF; height: 50px; width: 50px; outline: 0.5px solid;";
     grid_div.addEventListener("mouseover", () =>{
         grid_div.style.cssText = `background: ${color}; height: 50px; width: 50px; outline: 0.5px solid;`;
-        console.log("turn Black")
     } );
     grid.appendChild(grid_div);
 }
