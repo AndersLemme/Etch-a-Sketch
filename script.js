b9ff8baee8d5c4607e004ad5693c4c5720aacff1
AndersLemme <a.lemme@hotmail.com>
2025-07-12 16:42:18 +0200

    Added randome button that filles inn random rgb color. Started on grayscale button, but decided to drop it because i use the nice wihteish and blackish color... I might come back and add it in the future.

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const white_btn = document.getElementById("white");
 white_btn.addEventListener("click", () =>{ color = "#D0B3FF";});
 const black_btn = document.getElementById("black");
 black_btn.addEventListener("click", () =>{ color = "#111820";});
+const rand_btn = document.getElementById("rand");
+rand_btn.addEventListener("click", () =>{ color = "rand"});
+
+//const gray_btn = document.getElementById("gray");
+//gray_btn.addEventListener("click", () =>{ color = "gray"});
+
 
 //define Grid
 const grid = document.querySelector(".grid");
@@ -26,10 +32,6 @@ slider.addEventListener("input", () =>{
 })
 slider.addEventListener("change", () =>{
     let val = Math.round(slider.value /5)*5;
-    console.log("activate create grid funcrion-- debugging");
-    console.log(val);
-    console.log(typeof val);
-
     createGrid(val);
 })
 
@@ -37,13 +39,24 @@ function createGrid(size){
     grid.innerHTML = ""; //clear grid before creating new one
 
     for(let i = 0; i < size*size; i++){
-        console.log("loop is running");
         const grid_div = document.createElement("div");
         grid_div.style.cssText = `background: #D0B3FF; height: ${(500/size).toString()}px; width: ${(500/size).toString()}px; outline: 0.5px solid;`;
         grid_div.addEventListener("mouseover", () =>{
-            grid_div.style.cssText = `background: ${color}; height: ${(500/size).toString()}px; width: ${(500/size).toString()}px; outline: 0.5px solid;`;
-        } );
+            if( color == "rand"){
+                grid_div.style.cssText = `background: ${(randRGB())}; height: ${(500/size).toString()}px; width: ${(500/size).toString()}px; outline: 0.5px solid;`;
+            }else{
+                grid_div.style.cssText = `background: ${color}; height: ${(500/size).toString()}px; width: ${(500/size).toString()}px; outline: 0.5px solid;`;
+            }
+        });
 
         grid.appendChild(grid_div);
     }
+}
+
+function randRGB(){
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+
+    return `rgb(${r},${g},${b})`
 }
